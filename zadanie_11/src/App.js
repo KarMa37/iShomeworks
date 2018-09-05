c163bbdd592dd1cf646e6b24c1698c84e240575a
Marcin Karwacki <kreatywny20@wp.pl>
2018-09-05 22:58:32 +0200

    Import players table + add Fragment

--- zadanie_11/src/App.js
+++ zadanie_11/src/App.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import './App.css';
+import Players from './players';
 
 class App extends Component {
   render() {
-    return
+    return <Fragment>
+
+    </Fragment>
   }
 }
 
