ca4e748ac4a20870df3554cbf47fa10bbcdf5be5
Marcin Karwacki <kreatywny20@wp.pl>
2018-09-05 23:03:15 +0200

    Add heading and table with thead

--- zadanie_11/src/App.js
+++ zadanie_11/src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import Players from './players';
 
 class App extends Component {
-  render() {
-    return <Fragment>
-
-    </Fragment>
-  }
+    render() {
+        return <Fragment>
+            <h1>Players</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Points</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </Fragment>
+    }
 }
 
 export default App;
