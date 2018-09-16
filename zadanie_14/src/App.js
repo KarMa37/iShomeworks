d91bece31d402af8a1ffba633ed755d9e84af83e
Marcin Karwacki <kreatywny20@wp.pl>
2018-09-16 22:21:57 +0200

    Part 2 done

--- zadanie_14/src/App.js
+++ zadanie_14/src/App.js
@@ -1,29 +1,11 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
+import Players from './Players';
 
 class App extends Component {
 
     state = {
         players: [],
-        highlightingColor: ""
-    };
-
-    redColor = () => {
-        this.setState({
-            highlightingColor: "red"
-        })
-    };
-
-    blueColor = () => {
-        this.setState({
-            highlightingColor: "blue"
-        })
-    };
-
-    clearColor = () => {
-        this.setState({
-            highlightingColor: ""
-        })
     };
 
     componentDidMount() {
@@ -33,45 +15,8 @@ class App extends Component {
     };
 
     render() {
-        const arrayId = this.state.players.map(player => {
-            return player.id
-        });
-        const maxId = Math.max(...arrayId);
         return <Fragment>
-            <h1 className="center">Players</h1>
-            <div className="center">
-                <button className="red" onClick={this.redColor}>Red</button>
-                <button className="blue" onClick={this.blueColor}>Blue</button>
-                <button onClick={this.clearColor}>Clear</button>
-            </div>
-            <table className="center">
-                <thead>
-                <tr>
-                    <th>Username</th>
-                    <th>Points</th>
-                </tr>
-                </thead>
-                <tbody>
-                {this.state.players.map(player => {
-                    return <tr className={player.points > 100 ? this.state.highlightingColor : ''} key={player.id}>
-                        <td>{player.username}</td>
-                        <td>{player.points}</td>
-                    </tr>
-                })}
-                <tr>
-                    <th>Total</th>
-                    <th>{this.state.players.reduce((prev, next) => {
-                        return prev + next.points
-                    }, 0)}</th>
-                </tr>
-                <tr>
-                    <th>Average</th>
-                    <th>{this.state.players.reduce((prev, next) => {
-                        return prev + next.points
-                    }, 0) / maxId}</th>
-                </tr>
-                </tbody>
-            </table>
+            <Players players={this.state.players}/>
         </Fragment>
     }
 }
---
+++ zadanie_14/src/Players/Players.js
@@ -0,0 +1,71 @@
+import React, {Component, Fragment} from 'react';
+
+class Players extends Component {
+
+    state = {
+        highlightingColor: ""
+    };
+
+    redColor = () => {
+        this.setState({
+            highlightingColor: "red"
+        })
+    };
+
+    blueColor = () => {
+        this.setState({
+            highlightingColor: "blue"
+        })
+    };
+
+    clearColor = () => {
+        this.setState({
+            highlightingColor: ""
+        })
+    };
+
+    render() {
+        const arrayId = this.props.players.map(player => {
+            return player.id
+        });
+        const maxId = Math.max(...arrayId);
+        return <Fragment>
+            <h1 className="center">Players</h1>
+            <div className="center">
+                <button className="red" onClick={this.redColor}>Red</button>
+                <button className="blue" onClick={this.blueColor}>Blue</button>
+                <button onClick={this.clearColor}>Clear</button>
+            </div>
+            <table className="center">
+                <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Points</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.props.players.map(player => {
+                    return <tr className={player.points > 100 ? this.state.highlightingColor : ''} key={player.id}>
+                        <td>{player.username}</td>
+                        <td>{player.points}</td>
+                    </tr>
+                })}
+                <tr>
+                    <th>Total</th>
+                    <th>{this.props.players.reduce((prev, next) => {
+                        return prev + next.points
+                    }, 0)}</th>
+                </tr>
+                <tr>
+                    <th>Average</th>
+                    <th>{this.props.players.reduce((prev, next) => {
+                        return prev + next.points
+                    }, 0) / maxId}</th>
+                </tr>
+                </tbody>
+            </table>
+        </Fragment>
+    }
+}
+
+export default Players;
