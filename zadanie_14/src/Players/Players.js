250bb8fa5018b6545585919068505d351e0a28f0
Marcin Karwacki <kreatywny20@wp.pl>
2018-09-20 21:53:36 +0200

    Part 4 done

--- zadanie_14/src/Players/Players.js
+++ zadanie_14/src/Players/Players.js
@@ -4,7 +4,7 @@ import PlayerRow from './PlayerRow';
 class Players extends Component {
 
     state = {
-        highlightingColor: ""
+        highlightingColor: "",
     };
 
     redColor = () => {
@@ -25,6 +25,26 @@ class Players extends Component {
         })
     };
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const maxId = Math.max.apply(Math, this.props.players.map(player => player.id));
+        const newPlayer = {
+            id: maxId + 1,
+            username: this.state.inputValue,
+            points: Math.floor(Math.random()*201)
+        };
+        this.setState({
+            players: this.props.players.push(newPlayer),
+            inputValue: ""
+        })
+    };
+
+    onChange = event => {
+        this.setState({
+            inputValue: event.target.value
+        })
+    };
+
     render() {
         return <Fragment>
             <h1 className="center">Players</h1>
@@ -32,6 +52,10 @@ class Players extends Component {
                 <button className="red" onClick={this.redColor}>Red</button>
                 <button className="blue" onClick={this.blueColor}>Blue</button>
                 <button onClick={this.clearColor}>Clear</button>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.onChange} value={this.state.inputValue}/>
+                    <button>Add</button>
+                </form>
             </div>
             <table className="center">
                 <thead>
@@ -55,9 +79,9 @@ class Players extends Component {
                 </tr>
                 <tr>
                     <th>Average</th>
-                    <th>{this.props.players.reduce((prev, next) => {
+                    <th>{Math.round(this.props.players.reduce((prev, next) => {
                         return prev + next.points
-                    }, 0) / this.props.players.length}</th>
+                    }, 0) / this.props.players.length)}</th>
                 </tr>
                 </tbody>
             </table>
